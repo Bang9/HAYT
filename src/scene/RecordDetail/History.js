24d02e116de2b5120ec1d3cd43a972e6fcdd778c
kojongick <kojong0317@naver.com>
2017-07-24 17:53:18 +0900

    Record page
    1. selected emotion button design
    2. Record Remove modal -> Long Press

--- src/scene/RecordDetail/History.js
+++ src/scene/RecordDetail/History.js
@@ -1,10 +1,10 @@
 import React, {Component} from "react";
-import {Dimensions, Text, View,FlatList} from "react-native";
-import Button from "../../components/EmotionBar";
-import SelectedEmotion from "../../components/SelectedEmotion";
+import {Dimensions, Text, View,FlatList, TouchableOpacity, TouchableNativeFeedback} from "react-native";
 import API from "../../services/API"
 import firebase from '../../commons/Firebase'
 const {width,height} = Dimensions.get('window');
+import EmotionBar from "../../components/EmotionBar";
+import Modal from 'react-native-modal'
 
 const timeConverter = (timeStamp)=>{
     let a = new Date(parseInt(timeStamp));
@@ -30,6 +30,7 @@ class History extends Component {
     constructor(){
         super()
         this.state={
+            modalVisible:false,
             emotionData : null
         }
         this.data=null
@@ -48,6 +49,28 @@ class History extends Component {
             })
     }
 
+    emotionList(temp){
+        let str = []
+        for(var i =0; i<3; i++){
+            str[i] = temp[i].emotion
+        }
+        return(
+            <EmotionBar
+                emotions={str}
+                method={(emotion,check)=>this.toggle_emotion(emotion,check)}
+                ref={(refs)=>this.barRef[0]=refs}
+            />
+        )
+    }
+
+    show_modal(){
+        this.setState({modalVisible:true})
+    }
+    close_modal(){
+        this.setState({modalVisible:false})
+    }
+
+
     render(){
         return(
             <View>
@@ -57,19 +80,57 @@ class History extends Component {
                     <FlatList
                         data = {this.state.emotionData}
                         renderItem = { ({item,index})=> // renderItem return obj{item,index,sperator}
+                        <TouchableOpacity
+                        onLongPress = {() => this.show_modal() }>
                             <View style={{flex:1,borderWidth:1}}>
                                 <Text>{JSON.stringify(timeConverter(item.time))}</Text>
-                                <Text>{JSON.stringify(item.emotions)}</Text>
-                                <Text>{item.comment}</Text>
+                                {this.emotionList(item.emotions)}
                             </View>
+                        </TouchableOpacity>
                         }
                         keyExtractor={(item => item.time)}
                     />
                 }
+
+                <CommentModal
+                    modalVisible = {this.state.modalVisible}
+                    closeModal = {()=>this.close_modal()}
+                    onClick = {(comment)=>this.send_data(comment)}
+                />
             </View>
 
         )
+
     }
 }
 
 export default History;
+
+class CommentModal extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+            comment:''
+        }
+    }
+    close(){
+        this.setState({modalVisible:false})
+    }
+    render(){
+        return(
+            <Modal
+                isVisible={this.props.modalVisible}
+                onBackButtonPress={this.props.closeModal}
+                hideOnBack={true}>
+
+                <View style ={{borderRadius : 30,justifyContent:'center',alignItems:'center',backgroundColor:'#fff'}}>
+                    <TouchableNativeFeedback style={{width:300}}>
+                        <View style={{width :350, alignItems : "center"}}>
+                            <Text style={{fontSize:16,}}>기록 삭제하기</Text>
+                        </View>
+                    </TouchableNativeFeedback>
+                </View>
+            </Modal>
+        )
+    }
+}
