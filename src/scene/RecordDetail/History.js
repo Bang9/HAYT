02da7279282e4c806d9e4ed31d617f618fb123ba
kojongick <kojong0317@naver.com>
2017-07-25 18:00:01 +0900

    emotion array
    1. emotion array output.
    2. longPress
    3. accordion module connect

--- src/scene/RecordDetail/History.js
+++ src/scene/RecordDetail/History.js
@@ -1,10 +1,11 @@
 import React, {Component} from "react";
-import {Dimensions, Text, View,FlatList, TouchableOpacity, TouchableNativeFeedback} from "react-native";
+import {Dimensions, Text, View,FlatList, TouchableOpacity, TouchableNativeFeedback, ListView} from "react-native";
 import API from "../../services/API"
 import firebase from '../../commons/Firebase'
 const {width,height} = Dimensions.get('window');
 import EmotionBar from "../../components/EmotionBar";
 import Modal from 'react-native-modal'
+import Accordion from 'react-native-accordion';
 
 const timeConverter = (timeStamp)=>{
     let a = new Date(parseInt(timeStamp));
@@ -23,7 +24,7 @@ const timeConverter = (timeStamp)=>{
 
     let date = `${dateObj.year}/${dateObj.month}/${dateObj.date}`;
     let time = `${timeObj.hour}:${timeObj.min}:${timeObj.sec}`;
-    return {date:date,time:time}
+    return JSON.stringify({date:date,time:time})
 }
 
 class History extends Component {
@@ -35,6 +36,7 @@ class History extends Component {
         }
         this.data=null
     }
+
     componentWillMount(){
         let uid = 'userID'
         let ref = `users/${uid}/history`
@@ -45,20 +47,20 @@ class History extends Component {
                     obj[key].time = key
                     items.push(obj[key]) // recorded item push to array
                 }
+                items.reverse()
                 this.setState({emotionData:items})
             })
     }
 
     emotionList(temp){
-        let str = []
-        for(var i =0; i<3; i++){
-            str[i] = temp[i].emotion
+        let emotions = []
+        for(var i =0; i<temp.length; i++){
+            emotions[i] = temp[i].emotion
         }
         return(
             <EmotionBar
-                emotions={str}
-                method={(emotion,check)=>this.toggle_emotion(emotion,check)}
-                ref={(refs)=>this.barRef[0]=refs}
+                emotions={emotions}
+                method={()=>{}}
             />
         )
     }
@@ -81,26 +83,29 @@ class History extends Component {
                         data = {this.state.emotionData}
                         renderItem = { ({item,index})=> // renderItem return obj{item,index,sperator}
                         <TouchableOpacity
-                        onLongPress = {() => this.show_modal() }>
+                            onLongPress = {() => this.show_modal() }>
                             <View style={{flex:1,borderWidth:1}}>
-                                <Text>{JSON.stringify(timeConverter(item.time))}</Text>
+                                <Text>{timeConverter(item.time)}</Text>
                                 {this.emotionList(item.emotions)}
+                                <Text>{"코멘트 : "+item.comment}</Text>
+                                <TouchableOpacity
+                                    style={{backgroundColor:"#fff"}}
+                                      onPress ={()=>this.renderRow()}>
+                                    <Text>상세보기</Text>
+                                </TouchableOpacity>
                             </View>
                         </TouchableOpacity>
                         }
                         keyExtractor={(item => item.time)}
                     />
                 }
-
                 <CommentModal
                     modalVisible = {this.state.modalVisible}
                     closeModal = {()=>this.close_modal()}
                     onClick = {(comment)=>this.send_data(comment)}
                 />
             </View>
-
         )
-
     }
 }
 
