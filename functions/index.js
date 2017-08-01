8c932b78e23ef3a965aacd9758b672a73761e09d
bang9dev <gusrn1423@naver.com>
2017-08-02 01:44:47 +0900

    Main setting # Firebase cloud function update -> historyWatcher method updated (currentHistory data prototype update) # React native gif setting # MainTab.js -> Character & get current history and show

--- functions/index.js
+++ functions/index.js
@@ -9,9 +9,29 @@ exports.historyWatcher = functions.database.ref('/users/{uid}/history').onWrite(
         else return;
     }
 )
-
+/**
+ Obj structure
+ currentHistory = {
+    [
+        {
+            emotion : ~,
+            comment : ~,
+            timestamp : ~
+        },
+        {
+            emotion : ~,
+            comment : ~,
+            timestamp : ~
+        },
+        {
+            emotion : ~,
+            comment : ~,
+            timestamp : ~
+        }
+    ]
+ }
+ */
 function setCurrentHistory(snapshot){
-
     var uid = snapshot.ref.parent.key;
     var historyList = snapshot.val()
     var writeRef = admin.database().ref(`users/${uid}/currentHistory`)
@@ -19,7 +39,8 @@ function setCurrentHistory(snapshot){
     let updateItem = []
     if(historyList) {
         for (key in obj = historyList) {
-            updateItem.push(key) // recorded item push to array
+            obj[key].timeStamp = key
+            updateItem.push(obj[key]) // recorded item push to array
         }
         updateItem.reverse()
         updateItem = updateItem.splice(0,3);
--- src/scene/Main/MainTab.js
+++ src/scene/Main/MainTab.js
@@ -3,6 +3,7 @@ import {RefreshControl, ScrollView, Text, TouchableOpacity, View, StyleSheet,Dim
 import ActionButton from 'react-native-action-button';
 import Icon from 'react-native-vector-icons/Ionicons';
 import {AnimatedCircularProgress} from 'react-native-circular-progress'
+import API from '../../services/API'
 const {width,height} = Dimensions.get('window')
 
 class MainTab extends Component{
@@ -10,7 +11,20 @@ class MainTab extends Component{
         super(props);
         this.state={
             newMessage:null,
+            currentHistory:[{emotions:[{emotion:'',value:0},{emotion:'',value:0},{emotion:'',value:0}]}]
         }
+    }
+    componentWillMount(){
+        this.getCurrentHIstory();
+    }
+    getCurrentHIstory(){
+        let uid = API.get_uid()
+        let ref = `users/${uid}/currentHistory`
+        let emotionRef = null
+        API.getData(ref)
+            .then( (snapshot)=>{
+                this.setState({currentHistory:snapshot.val()})
+            })
     }
     render(){
         return(
@@ -19,6 +33,45 @@ class MainTab extends Component{
                 {/* Rest of the app comes ABOVE the action button component !*/
                     //  <Image source={require('../../img/background.png')} style={styles.backgroundImage} />
                 }
+                <View style={{flex:8,justifyContent:'center'}}>
+                    <Image
+                        style={{width:200,height:200}}
+                        source={{uri:'http://blogfiles.naver.net/MjAxNzAxMTZfMjU5/MDAxNDg0NTM4MTc1NDgw.NlXyGGYUuFv5aDbmJKyvVBCS7PkvAQlJjqwr5b7C6okg.ZowzZ8L-Ft_al1OXO9BDfFIUspwVFCUQCxrP-KBrajEg.GIF.gak05/%EB%8B%B9%ED%99%A9%ED%95%98%EB%8A%94_%EB%9D%BC%EC%9D%B4%EC%96%B8.gif'}}/>
+                </View>
+                <View style={{flex:2,flexDirection:'row',alignItems:'flex-start'}}>
+                    {
+                        this.state.currentHistory!=null &&
+                        this.state.currentHistory[0].emotions.map( (emotions,i) => {
+                            return(
+                                <AnimatedCircularProgress
+                                    key={i}
+                                    style={{margin:10}}
+                                    size={80}
+                                    width={10}
+                                    rotation={0}
+                                    friction={8}
+                                    fill={emotions.value * 19.999}
+                                    tintColor={'#ff8888'}
+                                    backgroundColor={'#ff888844'}>
+                                    {
+                                        (fill) => (
+                                            <View style={{width:80,height:80,position:'absolute',right:0,left:0,bottom:0,top:0,justifyContent:'center'}}>
+                                                <View >
+                                                    <Text style={{alignSelf:'center', fontSize:13}}>
+                                                        {emotions.emotion}
+                                                    </Text>
+                                                    <Text style={{alignSelf:'center', fontSize:10}}>
+                                                        {Math.round(fill/20)}
+                                                    </Text>
+                                                </View>
+                                            </View>
+                                        )
+                                    }
+                                </AnimatedCircularProgress>
+                            )
+                        })
+                    }
+                </View>
                 <ActionButton buttonColor="rgba(231,76,60,1)" verticalOrientation="down" position="right" autoInactive={false}>
                     <ActionButton.Item buttonColor='#9b59b6' title="New Task" onPress={() => console.log("notes tapped!")}>
                         <Icon name="md-create" style={styles.actionButtonIcon} />
@@ -30,16 +83,6 @@ class MainTab extends Component{
                         <Icon name="md-done-all" style={styles.actionButtonIcon} />
                     </ActionButton.Item>
                 </ActionButton>
-                <AnimatedCircularProgress
-                    style={{margin:10}}
-                    size={250}
-                    width={15}
-                    rotation={0}
-                    friction={8}
-                    fill={4 * 19.999}
-                    tintColor={'#ff8888'}
-                    backgroundColor={'#ff888844'}>
-                </AnimatedCircularProgress>
             </View>
         )
     }
